f0d7968c0bb867fcfddf9e7a696546adc442d118
xoron <rmaher84@gmail.com>
2022-03-28 20:09:20 +0200

    adding build functionality

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import merge from 'deepmerge';
+import { createBasicConfig } from '@open-wc/building-rollup';
+import typescript from '@rollup/plugin-typescript';
+import html from "@rollup/plugin-html";
+
+const baseConfig = createBasicConfig();
+
+export default merge(baseConfig, {
+  input: './src/index.html',
+  output: {
+      dir: 'dist',
+  },
+  plugins: [
+    html(),
+    typescript()
+  ]
+});
